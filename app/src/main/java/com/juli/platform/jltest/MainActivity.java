ccf4abc2c51b6912d09b32660536bcb3c667abb3
wulinqw <wulinqw@163.com>
2016-08-02 15:35:06 +0800

    Initial commit

---
+++ app/src/main/java/com/juli/platform/jltest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.juli.platform.jltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //TODO
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
